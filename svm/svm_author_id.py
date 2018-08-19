2d9230f5ef16e94d7eb776d1b232c6c32ffc5667
Tudor Paraschivescu <tudorp97@gmail.com>
2018-08-19 18:14:55 +0300

    Make predictions with the SVM classifier

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -9,22 +9,28 @@
 """
     
 import sys
+from sklearn import svm
 from time import time
+from sklearn.metrics import accuracy_score
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# Create and train the SVM classifier
+clf = svm.SVC(kernel="linear")
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
 
+# Make predictions for the test set
+t0 = time()
+pred = clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
 
-
-#########################################################
-### your code goes here ###
-
-#########################################################
-
-
+# Calculate accuracy
+accuracy = accuracy_score(labels_test, pred)
+print "SVM author identifier accuracy: %f" % accuracy
