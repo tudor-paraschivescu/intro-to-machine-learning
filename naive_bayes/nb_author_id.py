343a04bcf953b60dd3427d1995a1598f7edb47d8
Tudor Paraschivescu <tudorp97@gmail.com>
2018-08-19 16:05:26 +0300

    Make predictions with the Naive Bayes classifier and show its accuracy

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -9,25 +9,26 @@
     Sara has label 0
     Chris has label 1
 """
-    
+
 import sys
-from time import time
+import numpy as np
+from sklearn.naive_bayes import GaussianNB
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+# Create and train the Naive Bayes classifier
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
 
+# Make predictions for the test set
+computed_labels = clf.predict(features_test)
 
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+# Calculate accuracy
+correct_answers = np.sum(computed_labels == labels_test)
+accuracy = float(correct_answers) / len(labels_test)
+print "Naive Bayes author identifier accuracy: %f" % accuracy
