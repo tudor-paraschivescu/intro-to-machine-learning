5c91848d183c8b19ceb419c42c9b86c6046cc93d
Tudor Paraschivescu <tudorp97@gmail.com>
2018-09-02 18:10:35 +0300

    Make predictions with a custom Random Forest classifier

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python
 
 import matplotlib.pyplot as plt
+import sklearn.ensemble as ensemble
+import sklearn.tree as tree
+from sklearn.metrics import accuracy_score
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
 
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
+ATTEMPTS = 100
 
 ### the training data (features_train, labels_train) have both "fast" and "slow"
 ### points mixed together--separate them so we can give them different colors
@@ -26,17 +30,30 @@ plt.xlabel("bumpiness")
 plt.ylabel("grade")
 plt.show()
 ################################################################################
+accuracy_sum = 0
+max_accuracy = 0
 
+# create classifier
+clf = ensemble.RandomForestClassifier(n_estimators=2, max_features=None, min_samples_split=7)
 
-### your code here!  name your classifier object clf if you want the 
-### visualization code (prettyPicture) to show you the decision boundary
+for _ in xrange(1, ATTEMPTS):
+    # retrain the classifier
+    clf.fit(features_train, labels_train)
 
+    # predict the test labels
+    predictions = clf.predict(features_test)
 
+    # calculate the accuracy
+    accuracy = accuracy_score(labels_test, predictions)
 
+    # add the accuracy to the average
+    accuracy_sum += accuracy
 
+    # select the max accuracy
+    max_accuracy = max(max_accuracy, accuracy)
 
-
-
+print "Average accuracy of my classifier: %.3f%%" % (accuracy_sum * 100 / ATTEMPTS)
+print "Maximum accuracy of my classifier: %.3f%%" % (max_accuracy * 100)
 
 try:
     prettyPicture(clf, features_test, labels_test)
